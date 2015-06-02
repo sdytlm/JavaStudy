c1411a0864528984ad06c78724e5e26ec1df7da3
Min Li <sdytlm@gmail.com>
2015-06-01 17:49:08 -0700

    first commit

---
+++ code/ch03/Jiaogu.java
@@ -0,0 +1,23 @@
+import java.io.*;
+class Jiaogu 
+{
+	public static void main(String[] args) 
+	{
+		System.out.print("\n请输入一个数:");
+		try{
+			BufferedReader br = new BufferedReader ( 
+				new InputStreamReader( System.in ) );
+			String s = br.readLine();
+			int a = Integer.parseInt( s );
+			int cnt = 0;
+
+			while( a != 1 )
+			{
+				System.out.print( " " +a ); cnt++;
+				if( a%2==1 ) a = a*3+1; else a /= 2;
+			}
+			System.out.println(" " +a + "," + cnt);
+		}
+		catch(Exception e){}
+	}
+}
